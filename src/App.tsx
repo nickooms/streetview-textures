bc30035a69c7a7a56238fae382ebbbf81bfa1508
Nick Ooms <oomsni@gmail.com>
2023-06-06 23:30:23 +0200

    Added @react-google-maps/api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import { useState } from 'react';
-import { useStreetView } from './useStreetView';
-import './App.css';
+import { useMemo } from 'react';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+// import { useMemo } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const { status, data, error } = useStreetView('AIzaSyCg5I_JjwKeIkU5sum_tMDDaprwj3gr1vU');
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyAXskg1eQXzc0RM5FplzOY9-bDQN0Dd-7Q' /* process.env.REACT_APP_GOOGLE_API_KEY */,
+  });
+  const center = useMemo(() => ({ lat: 51.306623965868972, lng: 4.4057949201202788 }), []);
+
+  return (
+    <div className="App">
+      {!isLoaded ? <h1>Loading...</h1> : <GoogleMap mapContainerClassName="map-container" center={center} zoom={10} />}
+    </div>
+  );
+
+  /* const [map, setMap] = useState(null);
+  const [panorama, setPanorama] = useState(null);
+  const mapRef = useRef(null);
+  const panoramaRef = useRef(null);
+  const markt = useMemo(() => ({ lat: 51.306623965868972, lng: 4.4057949201202788 }), []);
+
+  useEffect(() => {
+    if (mapRef.current) {
+      setMap(
+        new google.maps.Map(mapRef.current, {
+          center: markt,
+          zoom: 19,
+        })
+      );
+    }
+  }, [mapRef, markt]);
+
+  useEffect(() => {
+    if (panoramaRef.current) {
+      setPanorama(
+        new google.maps.StreetViewPanorama(panoramaRef.current, {
+          position: markt,
+          pov: {
+            heading: 34,
+            pitch: 10,
+          },
+        })
+      );
+    }
+  }, [panoramaRef, markt]);
+
+  useEffect(() => {
+    if (map && panorama) {
+      map.setStreetView(panorama);
+    }
+  }, [map, panorama]);
 
   return (
     <>
-      <pre>
-        <code>{JSON.stringify({ status, data, error }, null, 2)}</code>
-      </pre>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <div id="map" ref={mapRef}></div>
+      <div id="pano" ref={panoramaRef}></div>
     </>
-  );
+  ); */
 }
 
 export default App;
