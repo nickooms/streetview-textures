62d66aeb0d5039654ee0d15d69e08fc3d2705419
Nick Ooms <oomsni@gmail.com>
2023-06-06 22:19:28 +0200

    changed location to hoevenen markt

--- src/test.js
+++ src/test.js
@@ -1,11 +1,12 @@
 export const initialize = () => {
-  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const markt = { lat: 51.306623965868972, lng: 4.4057949201202788 };
+  // const fenway = { lat: 42.345573, lng: -71.098326 };
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: fenway,
+    center: markt,
     zoom: 14,
   });
   const panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), {
-    position: fenway,
+    position: markt,
     pov: {
       heading: 34,
       pitch: 10,
